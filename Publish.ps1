30f8f58628d6b911ea9488615b399d0fa1e56b0a
Tylorhl <tylorhl@users.noreply.github.com>
2019-07-16 21:24:32 -0700

    Nojekyll

--- Publish.ps1
+++ Publish.ps1
@@ -6,6 +6,8 @@ rm "$PSScriptRoot/docs/*" -r
 
 cp ./bin/Release/netstandard2.0/publish/Tylorhl.ArtsyAshers/dist/* "$PSScriptRoot/docs/" -Recurse
 
+echo $null >> "$PSScriptRoot/docs/.nojekyll"
+
 rm ./bin/Release -r
 
 cd $workingDir
